374b66d7009a1eb4bd118a076f6976f338daacd0
hemanth <hemanthchallacs@gmail.con>
2023-01-30 22:20:05 +0530

    FoodOrderApp

---
+++ FoodOrderApp/src/Components/Layout/Header.js
@@ -0,0 +1,21 @@
+import { Fragment } from "react";
+import mealsImage from "../../assets/meals.avif";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <div className={classes.leftSection}>
+          <h1>MyMeals</h1>
+        </div>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
